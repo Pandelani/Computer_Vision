e13c1014babf5a70d177a34c33138338ef0f531c
Tony Huang <toby4548@gmail.com>
2017-02-08 18:08:29 +0100

    +Noise and Filter

--- Image_Processing_Matlab.m
+++ Image_Processing_Matlab.m
@@ -1,37 +1,61 @@
-%%Image Read
+%%
+%Image Read
 im=imread('yosemite-2048.jpg');
 imgreen=im(:,:,2);
 figure(1)
 imshow(imgreen);
 
-%%Show value of image function
+%%
+%Show value of image function
 line([1 515],[256 256],'color','r')
 figure(2)
 plot(imgreen(256,:))
 disp(size(imgreen))
 disp(class(imgreen))
 
-%%Blend two image with weighting(function at the end of file)
+%%
+%Blend two image with weighting(function at the end of file)
 dolphin = imread('dolphin.png');
 bicycle = imread('bicycle.png');
 figure(3)
 result = blend(dolphin, bicycle, 0.75);
 imshow(result);
 
-%%Image subtraction
+%%
+%Image subtraction
 figure(4)
 imshow(imabsdiff(dolphin, bicycle));
 
-%%Noise Gaussian Modeling
-noise=randn(size(bicycle)).*25;
+%%
+%Noise Gaussian Modeling
+noise_sigma=25;
+noise=randn(size(bicycle)).*noise_sigma;
 noise=uint8(noise);
+bicycle_noise = bicycle+noise;
 figure(5)
-imshow(bicycle+noise);
+imshow(bicycle_noise);
 
-%%function for blend
+%%
+%Gaussian filtering
+hsize=11;
+sigma=2;
+h=fspecial('gaussian',hsize,sigma);
+figure(6)
+surf(h);
+figure(7)
+imagesc(h);
+outim=imfilter(bicycle_noise,h);
+figure(8)
+imshow(outim)
+
+%%
+%function for blend
 function output = blend(a, b, alpha)
     output=alpha.*a+(1-alpha).*b;
 end
+
+
+
 
 
 
