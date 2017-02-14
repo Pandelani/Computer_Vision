6f04d6f111cc3e277689e19b23f0be1cf4449f60
unknown <>
2017-02-14 01:09:44 +0100

    update

--- find_template_2D.m
+++ find_template_2D.m
@@ -10,5 +10,12 @@ function [yIndex xIndex] = find_template_2D(template, img)
     yIndex=raw_yIndex-template_y+1;
     xIndex=raw_xIndex-template_x+1;
     
+    %{
+    另外一種寫法
+    [yRaw xRaw]=find(c==max(c(:)));
+    yIndex = yRaw - size(template,1)+1;
+    xIndex = xRaw - size(template,2)+1;
+    %}
+    
     
 end
